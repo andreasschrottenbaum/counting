a6618bb33e1d6890263197ea4382bac9e27ef795
Andreas Schrottenbaum <andreas.schrottenbaum@gmail.com>
2024-12-26 01:16:55 +0100

    removed bloat code

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-// https://vite.dev/config/
 export default defineConfig({
   base: '/counting/',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [svelte()],
 })
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   base: '/counting/',
-  build: {
-    outDir: 'dist',
-  },
   plugins: [svelte()],
 })
